4228feb0df986a44fd3c16b0104b0a2abac101e5
ait hmad oussama <aithmadoussama458@gmail.com>
2026-04-16 16:41:45 +0100

    lab 4

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 import pkg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  user: 'utilisateur',
+  host: 'localhost',
+  database: 'bibliotheque_db',
+  password: 'mot_de_passe', // ⚠️ doit être une string
+  port: 5432,
   max: 10,
   idleTimeoutMillis: 30000
 });
